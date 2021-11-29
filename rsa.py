18ab213f5594f63d54763e8e3bd2d6986569df17
beimnet777 <91521577+beimnet777@users.noreply.github.com>
2021-11-29 04:40:08 -0800

    adding functions for encoding decoding abd generating a number that will be used for genrating private and public key

--- rsa.py
+++ rsa.py
@@ -21,3 +21,37 @@ def egcd(a, b):
     else:
         gcd, x, y = egcd(b % a, a)
         return gcd, y - (b // a) * x, x
+
+def e_generating(n2):
+    x = randint(2, 150)
+    for i in range(n2 - x, 1, -1):
+        GCD, Z, L = egcd(i, n2)
+        if GCD == 1:
+            return i
+    return n2 - 1
+
+
+def encoding(str1, e, n):
+    st = ''
+    for i in str1:
+        st += str(ord(i))
+    num = int(st)
+ 
+    result = 1      
+    while e > 0:
+        if (e%2 == 1):
+            result = (result * num)%n
+        e = e >> 1
+        num = (num * num)% n
+    return result
+
+
+
+def decode(m, d, n):
+    result = 1 
+    while d > 0:
+        if (d%2 == 1):
+            result = (result * m)%n
+        d = d >> 1
+        m = (m * m)% n
+    return result
